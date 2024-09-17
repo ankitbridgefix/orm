2a91afb8052b740ca74ceaa127738144c4a36321
ankitbridgefix <132273809+ankitbridgefix@users.noreply.github.com>
2024-09-17 19:40:43 +0530

    Update settings.py for AWS deployment -by Shivansh

--- orm/settings.py
+++ orm/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-&tryg16wl83r3$_fnxhg$yo@x)@p#*(@^@z)6s(blmke7j*j*)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
