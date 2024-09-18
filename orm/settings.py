f3bfd5045ead634d7bd6ad62172f28cabb0509e2
shivansh <shivanshkaurav04@gmail.com>
2024-09-18 13:53:53 +0530

    updates in settings.py for the static files

--- orm/settings.py
+++ orm/settings.py
@@ -4,8 +4,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
-# STATIC_DIR = os.path.join(BASE_DIR,'static')
-# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -109,12 +109,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Include your local static directory
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where collectstatic will put files
 
 MEDIA_URL ='/media/'
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # This is where collectstatic will put files
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -123,11 +123,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'app.CustomUser'
 
-STORAGES = {
-    "static":{
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-        "OPTIONS": {
-            "location": 'static',
-        }
-    },
-}
+# STORAGES = {
+#     "static":{
+#         "BACKEND": "django.core.files.storage.FileSystemStorage",
+#         "OPTIONS": {
+#             "location": 'staticfiles',
+#         }
+#     },
+# }
