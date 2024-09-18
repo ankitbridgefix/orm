628ab453f4eec21cbdd45c773138167974b3f08d
shivansh <shivanshkaurav04@gmail.com>
2024-09-18 12:35:09 +0530

    settings STORAGES

--- orm/settings.py
+++ orm/settings.py
@@ -4,8 +4,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR = os.path.join(BASE_DIR,'static')
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# STATIC_DIR = os.path.join(BASE_DIR,'static')
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 # Quick-start development settings - unsuitable for production
@@ -113,8 +113,8 @@ STATIC_URL = 'static/'
 
 MEDIA_URL ='/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # This is where collectstatic will put files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # This is where collectstatic will put files
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -123,3 +123,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'app.CustomUser'
 
+STORAGES = {
+    "static":{
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "OPTIONS": {
+            "location": 'static',
+        }
+    },
+}
